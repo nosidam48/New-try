09570200b6a9a1ed9da565bb1aa8c27332657566
Mad Tea <43210154+nosidam48@users.noreply.github.com>
2019-05-23 12:26:45 +0200

    Updates

---
+++ precache-manifest.c74d8ab97b799d2e4af7d7b9f536acc8.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "666b0d2104895e77390667a68387f13c",
+    "url": "/New-try/index.html"
+  },
+  {
+    "revision": "42d81c8c87a99d2f5c4b",
+    "url": "/New-try/static/css/main.9167de39.chunk.css"
+  },
+  {
+    "revision": "219635517f5a5e3c9873",
+    "url": "/New-try/static/js/2.bc5fbade.chunk.js"
+  },
+  {
+    "revision": "42d81c8c87a99d2f5c4b",
+    "url": "/New-try/static/js/main.bd20baa3.chunk.js"
+  },
+  {
+    "revision": "adc25d382731a1176779",
+    "url": "/New-try/static/js/runtime~main.bf5230f3.js"
+  }
+]);
